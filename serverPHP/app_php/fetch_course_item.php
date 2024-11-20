0c02a3ae2f1b957a1113d041aebcf2b8de02446c
CorneCalitz <cornecalitz1@gmail.com>
2024-11-21 01:04:59 +0200

    final

---
+++ serverPHP/app_php/fetch_course_item.php
@@ -0,0 +1,40 @@
+<?php
+
+
+require_once "conn.php";
+
+$response = array();
+$course_item = array();
+
+//Fetch course data
+$stmt = $conn->query("SELECT id_course, course_name, course_desc FROM tbl_course");
+$rows = $stmt->num_rows;
+
+if ($rows > 0) {
+
+	while($row = $stmt->fetch_assoc()) {
+
+		$course_item["id"] = $row["id_course"];
+		$course_item["name"] = $row["course_name"];
+		$course_item["description"] = $row["course_desc"];
+
+		array_push($response, $course_item);
+	}
+
+}
+
+echo json_encode($response);
+$stmt->close();
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- serverPHP/app_php/login.php
+++ serverPHP/app_php/login.php
@@ -12,6 +12,7 @@ if (!empty($_POST["name"]) && !empty($_POST["password"])) {
 	$name = $_POST["name"];
 	$password = $_POST["password"];
 
+	//Get password to verify
 	$stmt = $conn->prepare("SELECT password FROM tbl_user WHERE name=?");
 	$stmt->bind_param("s", $name);
 	$stmt->execute();
@@ -35,7 +36,6 @@ if (!empty($_POST["name"]) && !empty($_POST["password"])) {
 			$stmt->bind_result($id_user, $name);
 			$stmt->fetch();
 			 
-
 			//Fetch user's profile
 			$stmt = $conn->prepare("SELECT id_profile, tests_taken, tests_passed FROM tbl_profile WHERE tbl_user_id_user=?");
 			$stmt->bind_param("i", $id_user);
@@ -46,12 +46,13 @@ if (!empty($_POST["name"]) && !empty($_POST["password"])) {
 
 			$stmt->close();
 
-			//TODO: remove the id's being fetched, remember to do so in the corresponding class models.
-			$response["user"]["id"] = $id_user;
 			$response["user"]["name"] = $name;
+
 			$response["profile"]["profile_id"] = $id_profile;
 			$response["profile"]["tests_taken"] = $tests_taken; 
 			$response["profile"]["tests_passed"] = $tests_passed;
+
+
 			$result->setErrorStatus(false);
 			$result->setMessage("Login success");
 		} else {
@@ -63,6 +64,7 @@ if (!empty($_POST["name"]) && !empty($_POST["password"])) {
 		$result->setMessage("Invalid credentials");
 	}
 } else { 
+	//redundant
 	$result->setErrorStatus(true);
 	$result->setMessage("Missing parameters");
 }
